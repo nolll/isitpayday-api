6c5c364f28fff590b3a809e617e9bc7d68b27470
Henrik Severinson <henriks@gmail.com>
2017-03-13 21:06:27 +0100

    Removed custom json serialization

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -17,7 +17,7 @@ namespace Web
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-            ConfigFormatters(GlobalConfiguration.Configuration);
+            //ConfigFormatters(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
