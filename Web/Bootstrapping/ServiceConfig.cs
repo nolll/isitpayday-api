26877db0e1cd168561c7fcbffc42f5eb8d04b61b
nolll <henriks@gmail.com>
2020-05-03 18:26:45 +0200

    updated all nuget packages

--- Web/Bootstrapping/ServiceConfig.cs
+++ Web/Bootstrapping/ServiceConfig.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
