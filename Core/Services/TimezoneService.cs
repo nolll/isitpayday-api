10c07db2524623ade00166450d882a21cb343074
Henrik <henriks@gmail.com>
2022-10-24 00:17:35 +0200

    Trying to fix windows timezone problem

--- Core/Services/TimezoneService.cs
+++ Core/Services/TimezoneService.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Core.Exceptions;
 using TimeZoneConverter;
@@ -8,8 +7,6 @@ namespace Core.Services;
 
 public static class TimezoneService
 {
-    private static IEnumerable<TimeZoneInfo> WindowsTimezones => TimeZoneInfo.GetSystemTimeZones();
-
     public static TimeZoneInfo GetTimezone(string id)
     {
         var timezone = GetWindowsTimezone(id);
@@ -25,7 +22,8 @@ public static class TimezoneService
 
     private static TimeZoneInfo GetWindowsTimezone(string id)
     {
-        return WindowsTimezones.FirstOrDefault(o => o.Id == id);
+        var foundId = TZConvert.KnownWindowsTimeZoneIds.FirstOrDefault(o => o == id);
+        return foundId == null ? null : TimeZoneInfo.FindSystemTimeZoneById(foundId);
     }
 
     private static TimeZoneInfo GetIanaTimezone(string ianaId)
