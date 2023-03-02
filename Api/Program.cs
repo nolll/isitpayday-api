d6a37c5a4e74a4ba79cc28a62842fcbe5665b8e3
Henrik <henriks@gmail.com>
2023-03-02 23:48:59 +0100

    New style program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,25 +1,85 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Api.Middleware;
+using Api.Settings;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace Api;
+var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT");
+var settings = builder.Configuration.Get<AppSettings>() ?? new AppSettings();
+var isDev = builder.Environment.IsDevelopment();
+var isProd = isDev;
 
-public class Program
+if (!string.IsNullOrEmpty(port))
+    builder.WebHost.UseUrls("http://*:" + port);
+
+builder.Services.AddResponseCompression(options =>
 {
-    public static void Main(string[] args)
-    {
-        CreateWebHostBuilder(args).Build().Run();
-    }
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+});
 
-    public static IHostBuilder CreateWebHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    var port = Environment.GetEnvironmentVariable("PORT");
+builder.Services.AddLogging(logging =>
+{
+    if (settings.Logging.Loggers.Debug)
+        logging.AddDebug();
 
-                    if(!string.IsNullOrEmpty(port))
-                        webBuilder.UseStartup<Startup>().UseUrls("http://*:" + port);
-                    else
-                        webBuilder.UseStartup<Startup>();
-                });
+    if (settings.Logging.Loggers.Console)
+        logging.AddConsole();
+
+    logging.SetMinimumLevel(settings.Logging.LogLevel);
+});
+
+builder.Services.AddSingleton(settings);
+
+builder.Services.AddMvc(options =>
+{
+    options.EnableEndpointRouting = false;
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+    var xmlFile = $"{assemblyName}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "IsItPayday Api", Version = "v1" });
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+app.UseResponseCompression();
+
+if (isDev)
+    app.UseDeveloperExceptionPage();
+
+if (isProd)
+{
+    app.UseHsts();
+    app.UseHttpsRedirection();
 }
+
+//if (isDev)
+//{
+//    var errorUrl = $"/{Routes.Error}";
+//    app.UseStatusCodePagesWithReExecute(errorUrl);
+//    app.UseExceptionHandler(errorUrl);
+//    app.UseMiddleware<ExceptionLoggingMiddleware>();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Version 1"); });
+
+app.UseMiddleware<SecurityHeadersMiddleware>();
+app.UseMvc();
+app.UseStaticFiles();
+
+app.Run();
